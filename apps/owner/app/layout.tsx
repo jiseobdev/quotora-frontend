bedf31c320aa7e685ede5b058722d7e7637aa975
jiseobdev <jiseob.kim@me.com>
2025-03-15 14:05:38 +0900

    feat: Add login handler

--- apps/owner/app/layout.tsx
+++ apps/owner/app/layout.tsx
@@ -1,5 +1,16 @@
 import clsx from "clsx";
-import { NavLink, Outlet } from "react-router";
+import { NavLink, Outlet, redirect } from "react-router";
+import type { Route } from "./+types/layout";
+import { accessTokenCookie } from "./auth.server";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const cookieHeader = request.headers.get('Cookie');
+  const accessToken = await accessTokenCookie.parse(cookieHeader);
+
+  if (!accessToken) {
+    return redirect('/signin');
+  }
+}
 
 export default function Layout() {
   return (
