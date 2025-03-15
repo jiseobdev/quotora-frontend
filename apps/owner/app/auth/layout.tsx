aa5808b9e2ea29459fb698526e2dac7db013b073
jiseobdev <jiseob.kim@me.com>
2025-03-15 14:14:11 +0900

    feat: Add handle auth route logic on loaders

---
+++ apps/owner/app/auth/layout.tsx
@@ -0,0 +1,16 @@
+import { Outlet, redirect } from "react-router";
+import type { Route } from "./+types/layout";
+import { accessTokenCookie } from "~/auth.server";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const cookieHeader = request.headers.get('Cookie');
+  const accessToken = await accessTokenCookie.parse(cookieHeader);
+
+  if (accessToken) {
+    return redirect('/');
+  }
+}
+
+export default function Layout() {
+  return <Outlet />;
+}
--- apps/owner/app/routes.ts
+++ apps/owner/app/routes.ts
@@ -1,13 +1,15 @@
 import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-
-  route('signin', './auth/signin.tsx'),
-  route('signout', './auth/signout.tsx'),
-  route('signup', './auth/signup.tsx'),
+  layout('./auth/layout.tsx', [
+    route('signin', './auth/signin.tsx'),
+    route('signout', './auth/signout.tsx'),
+    route('signup', './auth/signup.tsx'),
+  ]),
 
   layout('./layout.tsx', [
+    index('./index.tsx'),
+
     ...prefix('projects', [
       index('./projects/list.tsx'),
       route(':id', './projects/details.tsx')
