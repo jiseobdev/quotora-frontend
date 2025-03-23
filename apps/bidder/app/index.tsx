7dc6bcaec5e8fc8ec5844ca0a4cec8c8bfcf694c
jiseobdev <jiseob.kim@me.com>
2025-03-23 13:25:22 +0900

    fix: Fix initial route

--- apps/bidder/app/index.tsx
+++ apps/bidder/app/index.tsx
@@ -1,3 +1,8 @@
+import { redirect } from "react-router";
+
+export async function loader() {
+  return redirect('/projects');
+}
 export default function Index() {
   return null;
 }
--- apps/owner/app/index.tsx
+++ apps/owner/app/index.tsx
@@ -1,3 +1,9 @@
+import { redirect } from "react-router";
+
+export async function loader() {
+  return redirect('/projects');
+}
+
 export default function Index() {
   return null;
 }
