03e0209e984e295c3961326f80e3db6c1114bc73
jiseobdev <jiseob.kim@me.com>
2025-03-26 17:02:31 +0900

    fix: Add user validation condition

--- apps/owner/app/auth/layout.tsx
+++ apps/owner/app/auth/layout.tsx
@@ -1,12 +1,15 @@
 import { Outlet, redirect } from "react-router";
 import type { Route } from "./+types/layout";
 import { accessTokenCookie } from "~/auth.server";
+import { fetchCurrentUser } from "~/lib/fetch";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const cookieHeader = request.headers.get('Cookie');
   const accessToken = await accessTokenCookie.parse(cookieHeader);
 
-  if (accessToken) {
+  const user = await fetchCurrentUser();
+
+  if (accessToken && user) {
     return redirect('/projects');
   }
 }
