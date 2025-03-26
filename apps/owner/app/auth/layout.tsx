a6aef00bc76611c2105551efbefdffce5f4f1761
jiseobdev <jiseob.kim@me.com>
2025-03-26 17:43:28 +0900

    fix: Fix error on not signed in

--- apps/owner/app/auth/layout.tsx
+++ apps/owner/app/auth/layout.tsx
@@ -4,12 +4,14 @@ import { getAccessToken } from "~/auth.server";
 import { fetchCurrentUser } from "~/lib/fetch";
 
 export async function loader({ request }: Route.LoaderArgs) {
-  const token = await getAccessToken(request);
-  const user = await fetchCurrentUser(token);
+  try {
+    const token = await getAccessToken(request);
+    const user = await fetchCurrentUser(token);
 
-  if (token && user) {
-    return redirect('/projects');
-  }
+    if (token && user) {
+      return redirect('/projects');
+    }
+  } catch (error: unknown) { }
 }
 
 export default function Layout() {
